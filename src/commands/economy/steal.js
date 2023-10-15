d140bbfd9fbcb010de9a92e5041ee898cc6cdda2
Xentradi <ikaika@xentradi.com>
2023-10-15 09:56:51 -1000

    spice up steal command text

--- src/commands/economy/steal.js
+++ src/commands/economy/steal.js
@@ -1,5 +1,5 @@
 const {SlashCommandBuilder} = require('discord.js');
-const stealCash = require('../../modules/economy/tranfers/stealCash'); // Adjust the path as necessary
+const stealCash = require('../../modules/economy/tranfers/stealCash');
 const {convertToSeconds} = require('../../utils/calculate');
 const {createEmbed} = require('../../utils/embedUtils');
 
@@ -7,37 +7,62 @@ module.exports = {
   cooldown: convertToSeconds('3h'),
   data: new SlashCommandBuilder()
     .setName('steal')
-    .setDescription('Attempt to steal cash from another user.')
+    .setDescription(
+      'Embark on a daring heist to snatch cash from a fellow member.'
+    )
     .addUserOption(option =>
       option
-        .setName('target')
-        .setDescription('Person you want to steal from')
+        .setName('victim')
+        .setDescription('The unsuspecting member you aim to rob')
         .setRequired(true)
     )
     .addIntegerOption(option =>
       option
         .setName('amount')
-        .setDescription('Amount of cash you want to steal')
+        .setDescription('The pile of cash you have your eyes set on')
         .setRequired(true)
     ),
 
   async execute(interaction) {
     await interaction.deferReply();
 
-    const target = interaction.options.getUser('target');
+    const victim = interaction.options.getUser('victim');
     const amount = interaction.options.getInteger('amount');
 
     const data = await stealCash(
       interaction.user.id,
-      target.id,
+      victim.id,
       interaction.guildId,
       amount
     );
 
+    const victoryMessage = [
+      'Smooth moves, master thief!',
+      "You're a natural! The heist went perfectly.",
+      'Silent and deadly, they never saw it coming!',
+      'Looks like crime does pay... this time!',
+    ];
+
+    const defeatMessage = [
+      'Busted! They caught you in the act.',
+      'Oops! Looks like you tripped the alarms.',
+      'You might want to rethink your life choices...',
+      "Jail's not fun, is it? Better luck next time!",
+    ];
+
+    const randomMessage = data.successful
+      ? victoryMessage[Math.floor(Math.random() * victoryMessage.length)]
+      : defeatMessage[Math.floor(Math.random() * defeatMessage.length)];
+
     if (!data) {
-      interaction.editReply(
-        "Couldn't process your request. Ensure both players exist and you're not stealing more than the target has."
-      );
+      const embedOptions = {
+        title: 'Heist Error',
+        description:
+          "Your heist didn't go as planned. Maybe the treasure chest was empty?",
+        color: '#FF8C00', // Suggesting an orange for error
+      };
+      const responseEmbed = createEmbed(embedOptions);
+      interaction.editReply({embeds: [responseEmbed]});
       return;
     }
 
@@ -45,20 +70,22 @@ module.exports = {
 
     if (data.successful) {
       embedOptions = {
-        title: '🎉 Successful Heist!',
-        description: `${
+        title: 'Heist Report: Success!',
+        description: `${randomMessage}\n${
           interaction.user.username
-        } managed to steal $${data.amountStolen.toLocaleString()} from ${
-          target.username
+        } managed to swipe $${data.amountStolen.toLocaleString()} from ${
+          victim.username
         }.`,
         color: '#33CC33',
       };
     } else {
       embedOptions = {
-        title: '❌ Failed Heist!',
-        description: `${interaction.user.username} failed to steal from ${
-          target.username
-        } and had to pay a penalty of $${data.penalty.toLocaleString()}.`,
+        title: 'Heist Report: Busted!',
+        description: `${randomMessage}\n${
+          interaction.user.username
+        } was caught trying to steal from ${
+          victim.username
+        } and faced a fine of $${data.penalty.toLocaleString()}.`,
         color: '#FF3333',
       };
     }
