25e48c2678bb52a3cc1becd1e77d4f3fd30f01c2
Xentradi <ikaika@xentradi.com>
2023-10-16 15:31:57 -1000

    restored working free blackjack command

--- src/modules/economy/games/blackjackGame.js
+++ src/modules/economy/games/blackjackGame.js
@@ -6,26 +6,74 @@ const {
   calculateValue,
 } = require('../../../utils/blackjackUtils'); // You might need to create blackjackUtils.js for utility functions
 
+/**
+ * Performs a blackjack game, updates the player's cash balance based on the result, and returns detailed information about the operation.
+ *
+ * @async
+ * @function
+ * @param {string} userId - The ID of the user playing the blackjack game.
+ * @param {string} guildId - The ID of the guild (server) where the blackjack game is performed.
+ * @param {number} betAmount - The amount of cash the player bets.
+ * @returns {Promise<Object>} An object containing details about the blackjack game outcome, whether the player won, lost, or pushed, the prize or loss amount, and updated player balances.
+ *
+ * @throws Will throw an error if there are issues in updating the player's cash balance.
+ *
+ * @typedef {Object} BlackjackResult
+ * @property {boolean} success - Indicates whether the blackjack operation was successful.
+ * @property {string} playerOutcome - The outcome of the player's game ('win', 'lose', 'push').
+ * @property {Array<string>} playerCards - The cards dealt to the player.
+ * @property {Array<string>} dealerCards - The cards dealt to the dealer.
+ * @property {number} playerTotal - The total points of player’s cards.
+ * @property {number} dealerTotal - The total points of dealer’s cards.
+ * @property {number} betAmount - The amount of cash the player bet.
+ * @property {number} prize - The prize amount if the player wins, or the loss amount (negative) if the player loses, 0 if push.
+ * @property {number} playerBalanceBefore - The player's cash balance before the blackjack game.
+ * @property {number} playerBalanceAfter - The player's cash balance after the blackjack game and balance update.
+ * @property {string} [message] - Additional message, e.g., errors or insufficient funds notification.
+ */
+
 module.exports = async function blackjackGame(userId, guildId, betAmount) {
+  // Your game logic here
+  // Example:
   const player = await Player.findOne({userId, guildId});
 
-  // ... (Your existing logic for the game, including handling bets, creating and shuffling the deck, and determining the winner)
-
-  // You might want to return an object similar to this:
-  return {
-    success: true,
-    embedOptions: {
-      title: 'Blackjack Result',
-      description: 'Your game result goes here',
-      fields: [
-        {
-          name: 'Game Outcome',
-          value: 'Your game outcome goes here',
-          inline: false,
-        },
-        // ... any other fields you want in the embed
-      ],
-    },
-    // ... any other data you want to return
+  const result = {
+    success: false,
+    playerOutcome: '',
+    playerCards: [],
+    dealerCards: [],
+    playerTotal: 0,
+    dealerTotal: 0,
+    betAmount,
+    prize: 0,
+    playerBalanceBefore: player?.cash || 0,
+    playerBalanceAfter: player?.cash || 0,
   };
+
+  if (!player) {
+    result.message = 'Player not found in the database.';
+    return result;
+  }
+
+  if (player.cash < betAmount) {
+    result.message = 'Insufficient funds to place the bet.';
+    return result;
+  }
+
+  // More game logic here
+  // Dealing cards, player's choices to hit or stand, dealer's logic, determining win/lose/push, etc.
+
+  // Example of updating player's balance after determining the game outcome
+  const updateAmount = /* Calculate the amount based on game outcome */;
+
+  const updateResult = await balance.updatePlayerCash(player, updateAmount);
+
+  if (updateResult.success) {
+    // Set result properties like playerOutcome, playerCards, dealerCards, playerTotal, dealerTotal, prize, etc.
+    result.success = true;
+  } else {
+    result.message = 'An error occurred while processing the game.';
+  }
+
+  return result;
 };
