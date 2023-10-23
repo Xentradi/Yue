528aa7712c5f694adbf0fa1e6ea8d297a7ff2481
Xentradi <ikaika@xentradi.com>
2023-10-23 10:15:09 -1000

    fix sigmoid exp difficulty

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -5,10 +5,13 @@ module.exports.levelUp = function levelUp(level) {
   level += 1;
 
   const L = 10000; // max exp
-  const k = 0.1; // steepness of the curve
-  const x0 = 500; // Mid point level
+  const k = 0.01; // steepness of the curve
+  const x0 = 100; // Mid point level
 
   let sigmoidExp = L / (1 + Math.exp(-k * (level - x0)));
+  // Ensuring that the experience to level up is not too low
+  sigmoidExp = Math.max(sigmoidExp, level * 10);
+
   sigmoidExp = Math.round(sigmoidExp);
   console.log('next Level: ', level);
   console.log('expToLevel: ', sigmoidExp);
