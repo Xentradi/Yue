1ab448e4a1b804075bc23135721d8d3ea8e1c1a9
Xentradi <9624951+Xentradi@users.noreply.github.com>
2024-10-17 21:17:47 -1000

    Workspace update, jest install and config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',
+  ],
+};
